2bf4d9a810dc308d8340d9efa7cde4d86efda713
janhocevar <hockostudent@gmail.com>
2013-10-11 15:09:33 +0200

    Added javascript files

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
-<h1>Sublime git Test</h1>
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Jan Test</title>
+	<script src="js/jquery.js"></script>
+	<script src="js/jquery.jantest.js"></script>
+</head>
+<body>
 
-<p>Test</p>
 
-<h5>To je nov test</h5>
+
+</body>
+</html>
